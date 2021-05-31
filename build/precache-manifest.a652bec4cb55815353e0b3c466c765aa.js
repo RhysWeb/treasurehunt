f15060e26c4190f60d03a0ffefe154900da5e0ca
RhysWeb <Rhys25780@hotmail.com>
2021-05-31 23:55:24 +0100

    tried deleting the /build folder from the gitignore folder to see what happens

---
+++ build/precache-manifest.a652bec4cb55815353e0b3c466c765aa.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c3148481522c69c8418dfa6f5ec051b0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7e09719eba0e3310ffa0",
+    "url": "/static/css/main.af58ab5e.chunk.css"
+  },
+  {
+    "revision": "87c2a7cceb120ea88085",
+    "url": "/static/js/2.129e9bdd.chunk.js"
+  },
+  {
+    "revision": "2a3a23bf878d7187dc1e20049a44e9ba",
+    "url": "/static/js/2.129e9bdd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7e09719eba0e3310ffa0",
+    "url": "/static/js/main.07f1c14f.chunk.js"
+  },
+  {
+    "revision": "08af29ab710bac3e0a7d",
+    "url": "/static/js/runtime-main.6a645d27.js"
+  },
+  {
+    "revision": "c0efacd22d8615f87421be8a8e7f7de6",
+    "url": "/static/media/CosmoNeutral.c0efacd2.jpg"
+  },
+  {
+    "revision": "d4d035ce30ccd189c4feda3664ee1583",
+    "url": "/static/media/CosmoSad.d4d035ce.jpg"
+  },
+  {
+    "revision": "03368334d14b3dfef03df88ba08f8769",
+    "url": "/static/media/CosmoSmile2.03368334.jpg"
+  }
+]);
